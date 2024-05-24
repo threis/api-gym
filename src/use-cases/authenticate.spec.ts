977b5fa5b830914267d22351630cccf55b0587a2
Thiago <thiagoreis93@gmail.com>
2024-05-24 17:24:07 -0300

    add check-in use case

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -46,7 +46,7 @@ describe('Authenticate Use Case', () => {
       password_hash: await hash('123456', 6),
     })
 
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'john.doe@example.com',
         password: '123123',
